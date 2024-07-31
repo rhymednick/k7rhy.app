b57e76fa8df81cc1ec279f6bae04e15db379ab32
Rhy A. Mednick <rhymednick@users.noreply.github.com>
2024-07-31 15:54:42 -0700

    Improved procedure content layout for small screens (mobile) and shrunk the doc TOC width.

--- components/doc/doc-procedure.tsx
+++ components/doc/doc-procedure.tsx
@@ -29,7 +29,7 @@ export const DocProcedure: React.FC<DocProcedureProps> = (props: DocProcedurePro
         const image: React.ReactNode = step.image ? <Image src={step.image} alt={step.text} width={400} height={400} /> : null;
 
         return (
-            <div key={stepIndex} className="mb-4 pl-1 relative flex items-start">
+            <div key={stepIndex} className="mb-2 md:mb-4 pl-1 relative flex items-start">
             <div className="flex-shrink-0">
                 <div className="flex items-center justify-center w-8 h-8 rounded-full border-2 border-gray-400 text-center font-bold mr-2">
                 {stepIndex + 1}
@@ -49,16 +49,18 @@ export const DocProcedure: React.FC<DocProcedureProps> = (props: DocProcedurePro
                 </div>
                 {step.children}
                 {step.substeps?.map((substep: DocProcedureSubstep, substepIndex: number) => (
-                <div key={substepIndex} className="mb-4 mt-4 mr-2 ml-4">
+                <div key={substepIndex} className="mb-2 mt-2 ml-2 md:mb-4 md:mt-4 md:mr-2 md:ml-4">
 
-                    <div className="flex items-start">
-                        <div className="mr-4 w-3/4 align-text-top">
-                            <h4 className={cn("text-lg font-bold tracking-tight")}>
+                    <div className="flex flex-col md:flex-row md:items-start">
+                        <div className="mr-2 md:mr-4 w-3/4 align-text-top mb-2">
+                            <h4 className={cn("text-lg font-bold tracking-tight mb-2")}>
                                 ({String.fromCharCode(97 + substepIndex)}) {substep.text}
                             </h4>
+                            
                             {substep.description}
+                            
                         </div>
-                        <div>
+                        <div >
                             {substep.image && <Image src={substep.image} alt={substep.text} width={200} height={200} />}
                         </div>
                     </div>
@@ -78,7 +80,7 @@ export const DocProcedure: React.FC<DocProcedureProps> = (props: DocProcedurePro
             <h2 className={cn("text-2xl font-bold tracking-tight")}>
                 {props.title}
             </h2>
-            <div className='mb-4'>{description}</div>
+            <div className='mb-2 md:mb-4'>{description}</div>
             {props.docProcedureSteps?.map((step, index) => renderStep(step, index))}
         </div>
     );
