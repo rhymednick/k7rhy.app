bb5f3b187a68d0107f2bdc5dc4c07f226cc7bb6f
Rhy A. Mednick <rhymednick@users.noreply.github.com>
2024-08-17 19:25:55 -0700

    Fixed a heading level issue. Silly mistake.

--- components/doc/doc-procedure.tsx
+++ components/doc/doc-procedure.tsx
@@ -36,9 +36,9 @@ const renderSubsteps = (substeps?: DocProcedureSubstep[]): React.ReactNode => {
                     className="relative pl-8 xl:grid grid-cols-5 gap-16 before:content-[counter(substep,lower-alpha)] before:absolute before:left-0 before:flex before:items-center before:justify-center before:w-[calc(1.375rem+1px)] before:h-[calc(1.375rem+1px)] before:text-sm before:font-bold before:text-slate-700 before:rounded-md before:shadow-sm before:ring-1 before:ring-slate-900/5 before:bg-blue-100 dark:before:bg-slate-700 dark:before:text-slate-200 dark:before:ring-0 dark:before:shadow-none dark:before:highlight-white/5 pb-4 last:pb-0 after:absolute after:top-[calc(1.875rem+1px)] after:bottom-0 after:left-[0.6875rem] after:w-px after:bg-slate-200 dark:after:bg-slate-200/5"
                 >
                     <div className="col-span-2">
-                        <h5 className="text-base leading-6 text-slate-900 font-semibold mb-2 dark:text-slate-200">
+                        <h4 className="text-base leading-6 text-slate-900 font-semibold mb-2 dark:text-slate-200">
                             {substep.text}
-                        </h5>
+                        </h4>
                         <div className="flex flex-col md:flex-row md:items-start">
                             <div className="prose prose-slate prose-sm dark:prose-dark align-text-top mb-2 md:w-3/4 md:mr-4">
                                 {substep.description}
@@ -71,9 +71,9 @@ export const DocProcedure: React.FC<DocProcedureProps> = (props: DocProcedurePro
         return (
             <li key={stepIndex} style={{ counterIncrement: 'step 1' }} className="last:pb-0 relative pl-8 xl:grid grid-cols-5 gap-6 before:content-[counter(step)] before:absolute before:left-0 before:flex before:items-center before:justify-center before:w-[calc(1.375rem+1px)] before:h-[calc(1.375rem+1px)] before:text-sm before:font-bold before:text-slate-700 before:rounded-md before:shadow-sm before:ring-1 before:ring-slate-900/5 dark:before:bg-slate-700 dark:before:text-slate-200 dark:before:ring-0 dark:before:shadow-none dark:before:highlight-white/5 pb-2 after:absolute after:top-[calc(1.875rem+1px)] after:bottom-0 after:left-[0.6875rem] after:w-px after:bg-slate-200 dark:after:bg-slate-200/5">
                 <div className="col-span-2">
-                    <h4 className="text-lg leading-6 text-slate-900 font-semibold mb-2 dark:text-slate-200">
+                    <h3 className="text-lg leading-6 text-slate-700 font-semibold mb-2 dark:text-slate-200">
                         {step.text}
-                    </h4>
+                    </h3>
                     <div className="flex items-center ">
                         <div className="prose prose-slate prose-sm dark:prose-dark">
                             {description}
