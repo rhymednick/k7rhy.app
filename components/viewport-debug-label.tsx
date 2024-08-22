9751ddd12acdbfd1bd2788cd07aad80f68cf7f36
Rhy A. Mednick <rhymednick@users.noreply.github.com>
2024-08-21 18:12:20 -0700

    Fixed minor bug that was breaking the build.

--- components/viewport-debug-label.tsx
+++ components/viewport-debug-label.tsx
@@ -1,14 +1,10 @@
 "use client"
 import React from 'react';
-import { useWatchVariables } from '@/components/context/watch-variables-provider';
-
-import { createReactiveVariable } from '@/lib/createReactiveVariable';
 
 export default function ViewportDebugLabel() {
     if (process.env.NODE_ENV !== 'development') {
         return null; // Do not render in production mode
     }
-    let viewportSize = createReactiveVariable('viewportSize', 'string');
     return (
         <div className="mr-4 font-bold text-orange-500/40">
             <span className="inline md:hidden">[SM]</span>
