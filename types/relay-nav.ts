5a3fbfcae38ac2f66d4a4805d741aa2adfd3aeaa
Rhy Mednick <rhy.mednick@gmail.com>
2026-03-29 14:51:13 -0700

    style: format relay-nav types and config

--- types/relay-nav.ts
+++ types/relay-nav.ts
@@ -1,18 +1,18 @@
 export interface RelayNavItem {
-	title: string
-	slug: string // relative to model root, e.g. 'printing/parameters'
+    title: string;
+    slug: string; // relative to model root, e.g. 'printing/parameters'
 }
 
 export interface RelayNavSection {
-	title: string
-	items: RelayNavItem[]
+    title: string;
+    items: RelayNavItem[];
 }
 
 export interface RelayModelNav {
-	title: string
-	sections: RelayNavSection[]
+    title: string;
+    sections: RelayNavSection[];
 }
 
 export interface RelayNav {
-	[model: string]: RelayModelNav
+    [model: string]: RelayModelNav;
 }
